e8af87c532603c22b528ae5be78e2640d5aa9359
Rahmadani Pratiwi <rahmadanidp@gmail.com>
2019-05-17 10:28:57 +0700

    first

--- AnimationLesson/ViewController.swift
+++ AnimationLesson/ViewController.swift
@@ -13,8 +13,39 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let myNewView = UIView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        
+        myNewView.backgroundColor = UIColor.blue
+        view.addSubview(myNewView)
+        
+        UIView.animate(withDuration: 3) {
+//            print(myNewView.frame)
+            myNewView.frame = CGRect(x: 100, y: 300, width: 50, height: 50)
+            print(myNewView.frame)
+        }
+        
+        UIView.animate(withDuration: 3, delay: 1, usingSpringWithDamping: 1, initialSpringVelocity: 0, options: .curveLinear, animations: {
+            myNewView.frame = CGRect(x: 100, y: 500, width: 50, height: 50)
+            print(myNewView.frame)
+        }) { (isFinished) in
+             myNewView.frame = CGRect(x: 100, y: 500, width: 100, height: 100)
+            print(myNewView.frame)
+        }
+        
+        let mySecondView = UIView(frame:CGRect(x: 100, y: 100, width: 200, height: 50))
+        myNewView.backgroundColor = .blue
+        myNewView.alpha = 0
+        
+        view.addSubview(mySecondView)
+        UIView.animate(withDuration: 3, delay: 0, options: [.autoreverse, .repeat], animations: {
+            mySecondView.alpha = 1
+        }, completion: nil)
+        
+        print(mySecondView.frame)
+        
+        
     }
-
 
 }
 
